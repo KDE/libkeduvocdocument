b6b99da7f8f8ff3ce5ec7ea5ae5388b5a7dfbdfb
Ewald Arnold <ewald@ewald-arnold.de>
2002-06-02 15:45:58 +0000

    *** empty log message ***
    svn path=/trunk/kdeedu/libkdeedu/; revision=159019

---
+++ kdeeduui/kedusimpleentrydlg.h
@@ -0,0 +1,24 @@
+#ifndef kedusimpleentrydlg_included
+#define kedusimpleentrydlg_included
+
+#include <qobject.h>
+
+#include "kedusimpleentrydlgForm.h"
+
+class KEduSimpleEntryDlg : public KEduSimpleEntryDlgForm
+{
+    Q_OBJECT
+
+ public:
+
+
+
+
+
+};
+
+#endif // kedusimpleentrydlg_included
+
+/*
+   $Log: $
+*/
