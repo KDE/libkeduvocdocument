edeeefeecc31393d7703511e1acd24ca7fd5645e
Dirk Mueller <mueller@kde.org>
2003-05-01 11:53:54 +0000

    - use KDE_DEFAULT_CXXFLAGS - fix compilation
    svn path=/trunk/kdeedu/libkdeedu/; revision=223183

--- kdeeduui/kedusimpleentrydlg.cpp
+++ kdeeduui/kedusimpleentrydlg.cpp
@@ -1,7 +1,10 @@
-#include <kedusimpleentrydlg.h>
+#include "kedusimpleentrydlg.h"
 
 /*
    $Log$
+   Revision 1.2  2003/04/10 20:19:28  mlaurent
+   Includemoc
+
    Revision 1.1  2002/06/02 15:45:58  arnold
    *** empty log message ***
 
