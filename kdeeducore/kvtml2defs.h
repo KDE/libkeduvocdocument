5a0e10e4fd37852710061cf3de1c10553ded22e2
Jeremy Paul Whiting <jpwhiting@kde.org>
2007-06-26 01:53:48 +0000

    minor dtd fix, also added defs file for reader/writer to use
    svn path=/trunk/KDE/kdeedu/libkdeedu/; revision=680375

---
+++ kdeeducore/kvtml2defs.h
@@ -0,0 +1,92 @@
+/***************************************************************************
+                   Vocabulary Document for KDE Edu version 2
+    -----------------------------------------------------------------------
+    copyright     : (C) 2007 Jeremy Whiting <jeremy@scitools.com>
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef KVTML2DEFS_H
+#define KVTML2DEFS_H
+
+#define KVTML_TAG = "kvtml"
+#define KVTML_VER = "version"
+
+#define KVTML_INFO = "information"
+
+#define KVTML_GEN = "generator"
+#define KVTML_TITLE = "title"
+#define KVTML_AUTH = "author"
+#define KVTML_LIC = "license"
+#define KVTML_COM = "comment"
+
+#define KVTML_IDS = "identifiers"
+#define KVTML_ID = "identifier"
+#define KVTML_TYPE = "type"
+#define KVTML_LOCALE = "twoalpha"
+#define KVTML_NAME = "name"
+#define KVTML_SIZEHINT = "sizehint"
+#define KVTML_ARTICLE = "article"
+#define KVTML_DEF = "definite"
+#define KVTML_INDEF = "indefinite"
+#define KVTML_MALE = "male"
+#define KVTML_FEM = "female"
+#define KVTML_NEU = "neutral"
+#define KVTML_CONJ = "conjugation"
+#define KVTML_SING = "singular"
+#define KVTML_PLU = "plural"
+
+#define KVTML_TYPES = "types"
+
+#define KVTML_TENSES = "tenses"
+#define KVTML_TENSE = "tense"
+
+#define KVTML_USAGES = "usages"
+#define KVTML_USAGE = "usage"
+
+#define KVTML_LESSONS = "lessons"
+#define KVTML_LESSON = "lesson"
+#define KVTML_QUERY = "query"
+#define KVTML_CURRENT = "current"
+
+#define KVTML_ENTRY = "entry"
+#define KVTML_INACTIVE = "inactive"
+#define KVTML_INQUERY = "inquery"
+
+#define KVTML_TRANS = "translation"
+#define KVTML_TEXT = "text"
+#define KVTML_PRONUNC = "pronunciation"
+#define KVTML_FFF = "falsefriendfrom"
+#define KVTML_FFT = "falsefriendto"
+#define KVTML_FF = "falsefriend"
+#define KVTML_ANT = "antonym"
+#define KVTML_SYN = "synonym"
+#define KVTML_EX = "example"
+#define KVTML_PAR = "paraphrase"
+
+#define KVTML_COMPARISON = "comparison"
+#define KVTML_ABS = "absolute"
+#define KVTML_COMP = "comparative"
+#define KVTML_SUPER = "superlative"
+
+#define KVTML_MULTI = "multiplechoice"
+#define KVTML_CHOICE = "choice"
+
+#define KVTML_IMG = "image"
+#define KVTML_SND = "sound"
+
+#define KVTML_GRADE = "grade"
+#define KVTML_CURRENTGRADE = "currentgradeint"
+#define KVTML_COUNT = "count"
+#define KVTML_WRONG = "errorcount"
+#define KVTML_DATE = "date"
+
+#endif
+
