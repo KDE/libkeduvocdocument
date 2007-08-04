a965ffca15f98d003bc6f31640c4ffbaf9b9d721
Pino Toscano <pino@kde.org>
2007-08-04 00:00:20 +0000

    manually find what to extract, so the i18n "messages" of the test applications don't go into the libkdeedu.pot
    svn path=/trunk/KDE/kdeedu/libkdeedu/; revision=696151

--- Messages.sh
+++ Messages.sh
@@ -1,2 +1,8 @@
 #! /bin/sh
-$XGETTEXT `find . -name \*.h -o -name \*.cpp -o -name \*.cc` -o $podir/libkdeedu.pot 
+find extdate -maxdepth 1 -name \*.cpp >>list_libkdeedu
+find kdeeduui -maxdepth 1 -name \*.cpp >>list_libkdeedu
+find kdeeducore -maxdepth 1 -name \*.cpp -o -name \*.h >>list_libkdeedu
+find libscience -maxdepth 1 -name \*.cpp >>list_libkdeedu
+find extdate -maxdepth 1 -name \*.cpp >>list_libkdeedu
+$XGETTEXT --files-from=list_libkdeedu -o $podir/libkdeedu.pot
+rm list_libkdeedu
