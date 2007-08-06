75896676b996f08a7d187851cf9756c657757839
Pino Toscano <pino@kde.org>
2007-08-06 16:42:53 +0000

    kdeeducore got renamed to keduvocdocument
    svn path=/trunk/KDE/kdeedu/libkdeedu/; revision=697045

--- Messages.sh
+++ Messages.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 find extdate -maxdepth 1 -name \*.cpp >>list_libkdeedu
 find kdeeduui -maxdepth 1 -name \*.cpp >>list_libkdeedu
-find kdeeducore -maxdepth 1 -name \*.cpp -o -name \*.h >>list_libkdeedu
+find keduvocdocument -maxdepth 1 -name \*.cpp -o -name \*.h >>list_libkdeedu
 find libscience -maxdepth 1 -name \*.cpp >>list_libkdeedu
 find extdate -maxdepth 1 -name \*.cpp >>list_libkdeedu
 $XGETTEXT --files-from=list_libkdeedu -o $podir/libkdeedu.pot
