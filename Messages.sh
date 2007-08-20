2faac723c48876cdfc9482dc1b7eb7adf0686716
Laurent Montel <montel@kde.org>
2007-08-20 06:54:39 +0000

    Fix extract message
    svn path=/trunk/KDE/kdeedu/libkdeedu/; revision=702154

--- Messages.sh
+++ Messages.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
-find extdate -maxdepth 1 -name \*.cpp >>list_libkdeedu
+find extdate -maxdepth 1 -name \*.cpp >list_libkdeedu
 find kdeeduui -maxdepth 1 -name \*.cpp >>list_libkdeedu
-find keduvocdocument -maxdepth 1 -name \*.cpp -o -name \*.h >>list_libkdeedu
+find keduvocdocument -maxdepth 1 -name \*.cpp  >>list_libkdeedu
 find libscience -maxdepth 1 -name \*.cpp >>list_libkdeedu
 find extdate -maxdepth 1 -name \*.cpp >>list_libkdeedu
 $XGETTEXT --files-from=list_libkdeedu -o $podir/libkdeedu.pot
-rm list_libkdeedu
+rm -f list_libkdeedu
