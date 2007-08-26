3af1113c6488e9e805733f4cbdb19208ca2086c4
Frederik Gladhorn <gladhorn@kde.org>
2007-08-26 11:22:59 +0000

    copyright
    svn path=/trunk/KDE/kdeedu/libkdeedu/; revision=704823

--- keduvocdocument/keduvockvtmlreader.cpp
+++ keduvocdocument/keduvockvtmlreader.cpp
@@ -5,6 +5,7 @@
                           (C) 2001 The KDE-EDU team
                           (C) 2005 Eric Pignet <eric at erixpage.com>
                           (C) 2007 Peter Hedlund <peter.hedlund@kdemail.net>
+                          (C) 2007 Frederik Gladhorn <frederik.gladhorn@kdemail.net>
  ***************************************************************************/
 
 /***************************************************************************
