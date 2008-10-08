35151de5033e63bb4bc9c2437dee8edd90d96987
Frederik Gladhorn <gladhorn@kde.org>
2008-10-08 20:30:36 +0000

    fix license
    svn path=/trunk/KDE/kdeedu/libkdeedu/; revision=869358

--- keduvocdocument/keduvocwordflags.h
+++ keduvocdocument/keduvocwordflags.h
@@ -1,14 +1,15 @@
-//
-// C++ Interface: keduvocwordflags
-//
-// Description:
-//
-//
-// Author: David Capel <wot.narg@gmail.com>, (C) 2008
-//
-// Copyright: See COPYING file that comes with this distribution
-//
-//
+/***************************************************************************
+    Copyright 2008 David Capel <wot.narg@gmail.com>
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 
 #ifndef KEDUVOCWORDFLAGS_H
 #define KEDUVOCWORDFLAGS_H
