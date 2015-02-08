1f3d03feccb7fb2af47a53378d866dc0ff8baced
Rahul Chowdhury <rahulc93@gmail.com>
2015-02-09 02:03:13 +0530

    Added ContainerMimeData

---
+++ keduvocdocument/keduvoccontainermimedata.h
@@ -0,0 +1,43 @@
+/***************************************************************************
+
+    Copyright 2007 Frederik Gladhorn <frederik.gladhorn@kdemail.net>
+
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef KEDUVOCCONTAINERMIMEDATA_H
+#define KEDUVOCCONTAINERMIMEDATA_H
+
+#include "keduvoccontainer.h"
+
+#include <QMimeData>
+
+class KEduVocContainer;
+
+
+class KEduVocContainerMimeData : public QMimeData
+{
+    Q_OBJECT
+public:
+    void addContainer(KEduVocContainer* container);
+    QList<KEduVocContainer*> containerList() const;
+
+    KEduVocContainerMimeData();
+
+    ~KEduVocContainerMimeData();
+
+private:
+    class Private;
+    Private * const d;
+};
+
+
+#endif
