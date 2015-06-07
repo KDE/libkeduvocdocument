9b046eebe9aff1d938668f68f84da6b2a38795ab
Rahul Chowdhury <rahulc93@gmail.com>
2015-06-07 22:47:10 +0530

    Added KEduVocTranslator. Copied the Translator class from Parley, and added Scripting::Parley to it as Scripting::KEduVocDocument

---
+++ keduvocdocument/keduvoctranslator.cpp
@@ -0,0 +1,271 @@
+/***************************************************************************
+
+    Copyright 2008 Avgoustinos Kadis <avgoustinos.kadis@kdemail.net>
+
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+#include "keduvoctranslator.h"
+
+#include <QDebug>
+#include <QMessageBox>
+
+#include <keduvocvocabularymodel.h>
+#include <keduvocdocument.h>
+#include <keduvoclesson.h>
+#include <keduvocexpression.h>
+#include <keduvoctranslation.h>
+
+namespace Scripting
+{
+    KEduVocDocument::KEduVocDocument( ::KEduVocDocument * doc, KEduVocVocabularyModel * vocabularyModel ) 
+        : QObject(), 
+        m_translator( new KEduVocTranslator( this ) ),
+        m_doc( new ::KEduVocDocument( doc ) ),
+        m_vocabularyModel( vocabularyModel )
+    {
+    }
+
+    KEduVocDocument::~KEduVocDocument()
+    {
+        delete m_translator;
+        delete m_doc;
+        delete m_vocabularyModel;
+    }
+
+    void KEduVocDocument::callTranslateWord(const QString & word, const QString& fromLanguage, const QString& toLanguage)
+    {
+        emit translationStarted(word, fromLanguage, toLanguage);
+        emit translateWord(word, fromLanguage, toLanguage);
+        emit translationFinished(word, fromLanguage, toLanguage);
+    }
+
+    void KEduVocDocument::addTranslation(QString word, QString fromLanguage, QString toLanguage, QString translation)
+    {
+        if (m_translator)
+            m_translator->addTranslation(word, fromLanguage, toLanguage, translation);
+    }
+
+    QStringList KEduVocDocument::locales()
+    {
+        /// @todo Change it into a QMap property (Parley.languageCodes)
+        return QLocale().uiLanguages();
+    }
+
+    QString KEduVocDocument::localeName(QString locale)
+    {
+        return QLocale( locale ).nativeLanguageName( );
+    }
+
+    bool KEduVocDocument::open(QString filename)
+    {
+        QUrl url( QUrl::fromLocalFile(filename) );
+        qDebug() << url;
+        if (url.path().isEmpty()) {
+            return false;
+        }
+
+        close();
+
+        if (m_doc) {
+            m_doc->deleteLater();
+        }
+        m_doc = new ::KEduVocDocument(this);
+        emit documentChanged(m_doc);
+        //m_doc->setCsvDelimiter(Prefs::separator());
+
+        bool isSuccess = false, isError = false;
+
+        ::KEduVocDocument::ErrorCode ret = m_doc->open(url,  ::KEduVocDocument::FileDefaultHandling);
+        switch ( ret ) {
+            case ::KEduVocDocument::NoError :
+                isSuccess = true;
+                break;
+            case ::KEduVocDocument::FileLocked :
+            {
+                QMessageBox warningBox;
+                warningBox.setIcon( QMessageBox::Warning );
+                warningBox.setText( "Take Over Lock" );
+                warningBox.setInformativeText( "The vocabulary collection is locked by another process.  You can open the file if you take over the lock, but you will lose any changes from the other process.\n\nDo you want to take over the lock?\n" );
+                warningBox.setStandardButtons( QMessageBox::Yes | QMessageBox::No );
+                int exit = warningBox.exec();
+
+                if ( exit == QMessageBox::Yes ) { //attempt to steal lock
+
+                    ret = m_doc->open(url, ::KEduVocDocument::FileIgnoreLock);
+                    if ( ret == ::KEduVocDocument::NoError ) {
+                        qDebug() << "Lock stolen";
+                        isSuccess = true;
+                    } else {
+                        isError = true;
+                    }
+                } else { //Don't Steal continue work without saving !!!
+                }
+
+                break;
+            }
+            default:
+                isError = true;
+        }
+
+        if ( isSuccess ) {
+            qDebug() << "Open success.";
+            //m_parleyApp->editor()->updateDocument();
+
+        } else {
+            if ( isError ) {
+                QMessageBox errorBox;
+                errorBox.setIcon( QMessageBox::Critical );
+                errorBox.setText( "Open Collection" );
+                errorBox.setInformativeText( "Opening collection " + m_doc->url().url() + " resulted in an error: " + m_doc->errorDescription( ret ) );
+                errorBox.setStandardButtons( QMessageBox::Ok );
+                errorBox.exec();
+            }
+            m_doc->deleteLater();
+            emit documentChanged(0);
+            m_doc = 0;
+        }
+        return isSuccess;
+    }
+
+    void KEduVocDocument::close()
+    {
+        if (m_doc) {
+            emit documentChanged(0);
+            m_doc->deleteLater();
+            m_doc = 0;
+        }
+    }
+
+    /*KEduVocLesson* KEduVocDocument::activeLesson()
+    {
+        return m_vocabularyModel->lesson();
+    }*/
+
+    QVariantList KEduVocDocument::selectedEntries( QModelIndexList indexes )
+    {
+        QVariantList entries;
+
+        //get selected indexes and active lesson
+
+        //get the unique selected entries
+        QSet<KEduVocExpression*> kentries;
+        foreach(const QModelIndex & index, indexes) {
+            //             qDebug() << index.row() << index.data(Qt::DisplayRole);
+            KEduVocExpression * expr = qvariant_cast<KEduVocExpression*> (index.data(KEduVocVocabularyModel::EntryRole));
+            kentries << expr;
+        }
+
+        //convert them to Expression objects and add them to the QVariantList
+        foreach(KEduVocExpression * expr, kentries) {
+            //             Expression entry(expr);
+            //             qDebug() << entry.translationTexts();
+            KEduVocExpression * obj = new KEduVocExpression( *expr );
+            entries << qVariantFromValue( obj );
+        }
+
+        return entries;
+    }
+
+    QVariantList KEduVocDocument::selectedTranslations( QModelIndexList indexes )
+    {
+        QVariantList translations;
+
+        //get selected indexes and active lesson
+
+        //get the unique selected entries
+        QSet<KEduVocTranslation*> ktranslations;
+        //         const QModelIndex &index;
+        foreach(const QModelIndex & index, indexes) {
+            if (KEduVocVocabularyModel::columnType(index.column()) == KEduVocVocabularyModel::Translation) {
+                KEduVocExpression * expr = qvariant_cast<KEduVocExpression*> (index.data(KEduVocVocabularyModel::EntryRole));
+                ktranslations << expr->translation(KEduVocVocabularyModel::translation(index.column()));
+            }
+            //             qDebug() << index.row() << index.data(Qt::DisplayRole);
+        }
+
+        //convert them to Expression objects and add them to the QVariantList
+        foreach(KEduVocTranslation * tr, ktranslations) {
+            //             KEduVocTranslation transltion(tr);
+            //             qDebug() << entry.translationTexts();
+            KEduVocTranslation * obj = new KEduVocTranslation( *tr );
+            translations << qVariantFromValue(obj);
+        }
+
+        return translations;
+    }
+
+}
+
+
+
+
+
+class KEduVocTranslator::Private
+{
+public:
+    Private( QObject * parent );
+
+    QMap<QString, QSet<QString>*> m_translations;
+    QObject * m_parent;
+};
+
+KEduVocTranslator::Private::Private( QObject * parent = 0 )
+    : m_parent( parent )
+{
+}
+
+KEduVocTranslator::KEduVocTranslator()
+    : d( new Private )
+{
+}
+
+KEduVocTranslator::KEduVocTranslator(QObject * parent)
+    : d( new Private( parent ) )
+{
+}
+
+KEduVocTranslator::~KEduVocTranslator()
+{
+    delete d;
+}
+
+void KEduVocTranslator::addTranslation(QString word, QString fromLanguage, QString toLanguage, QString translation)
+{
+    if (word.trimmed() == "") return;
+
+    QString t = word + fromLanguage + toLanguage;
+    qDebug() << "Translation for " << word << "in cache: " << d->m_translations.contains(t);
+    if (!d->m_translations.contains(t)) {
+        d->m_translations[t] = new QSet<QString>();
+    }
+    d->m_translations[t]->insert(translation.simplified());
+}
+
+QSet<QString>* KEduVocTranslator::getTranslation(QString word, QString fromLanguage, QString toLanguage)
+{
+    if (word.isEmpty() || fromLanguage.isEmpty() || toLanguage.isEmpty()) return 0;
+
+    QString t = word + fromLanguage + toLanguage;
+    qDebug() << "Fetch translation " << word << "(" << fromLanguage << "to" << toLanguage << ")"
+             << "already in cache:" << d->m_translations.contains(t);
+    /*if (!d->m_translations.contains(t)) {
+        //FIXME Remove Parley dependancy and reimplement the commented code below
+        Scripting::KEduVocDocument * p = dynamic_cast<Scripting::KEduVocDocument*>(d->m_parent);
+        if (p) {
+            p->callTranslateWord(word, fromLanguage, toLanguage);
+        }
+    }*/
+
+    if (d->m_translations.contains(t))
+        return d->m_translations.value(t);
+    else
+        return 0;
+}
---
+++ keduvocdocument/keduvoctranslator.h
@@ -0,0 +1,361 @@
+/***************************************************************************
+
+    Copyright 2008 Avgoustinos Kadis <avgoustinos.kadis@kdemail.net>
+
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+#ifndef KEDUVOCTRANSLATOR_H
+#define KEDUVOCTRANSLATOR_H
+
+#include <QMap>
+#include <QStringList>
+#include <QStandardPaths>
+#include <QObject>
+
+#include <keduvocdocument.h>
+#include <keduvocvocabularymodel.h>
+#include <keduvocdocument_export.h>
+
+
+/**
+ Keeps the translated words
+ 
+ @author Avgoustinos Kadis <avgoustinos.kadis@kdemail.net>
+ */
+class KEDUVOCDOCUMENT_EXPORT KEduVocTranslator
+{
+public:
+    KEduVocTranslator();
+    KEduVocTranslator(QObject * parent);
+
+    ~KEduVocTranslator();
+
+    /**
+     * Stores the translation of @p word from language @p fromLanguage, to language @p toLanguage.
+     * @param word word that was translated
+     * @param fromLanguage language of @p word
+     * @param toLanguage language of @p translation
+     * @param translation translation of @p word
+     */
+    void addTranslation(QString word, QString fromLanguage, QString toLanguage, QString translation);
+
+    /**
+     * Returns a QStringList with all the translations of @p word from @p fromLanguage to @p toLanguage.
+     * This function will call the translateWord function of the translation scripts if this word
+     * wasn't translated before.
+     * @param word
+     * @param fromLanguage
+     * @param toLanguage
+     * @return QStringList with the translations (or an empty QStringList if no translations found)
+     */
+    QSet<QString>* getTranslation(QString word, QString fromLanguage, QString toLanguage);
+
+private:
+    class Private;
+    Private * const d;
+};
+
+
+/*namespace Editor
+{
+    class EditorWindow;
+}*/
+
+namespace Scripting
+{
+    /**
+     * @class KEduVocDocument
+     * @brief KEduVocDocument scripting class
+     *
+     * KEduVocDocument class is the main entry point of KEduVocDocument scripting classes. Through it you can access the Document class (KEduVocDocument.doc or KEduVocDocument.document) which provides functionality for viewing/modifying a %KEduVocDocument document , that means access lessons, entries, document languages etc.
+     *
+     * Here follows a list of possible usages of KEduVocDocument class:
+     *
+     * - Add a new Action to the script menu (see KEduVocDocument::newAction() function)
+     * - Add a new translation script (see KEduVocDocument::translateWord() signal)
+     * - Have direct access to the active lesson (see KEduVocDocument::activeLesson property)
+     * - Have access to various enumerations (see KEduVocDocument::Number, KEduVocDocument::Case, KEduVocDocument::Person, KEduVocDocument::Gender and KeduVocDocument::Definiteness enumerations)
+     * - Create a new %KEduVocDocument %Document (see KEduVocDocument::newDocument() function)
+     *
+     * Signals and Slots: To connect a script function (slot) to a signal you <b> just define a function with the same name as the signal </b> or use the KEduVocDocument.connect function:
+     * @code
+     * #how to connect a function to a signal (example with KEduVocDocument::translateWord() signal)
+     * def translateFromInternet(word,fromLang,toLang):
+     *     print "Translating from Internet!!.."
+     *
+     * KEduVocDocument.connect("translateWord(const QString &,const QString &,const QString &)",translateFromInternet)
+     * @endcode
+     *
+     *   @author Avgoustinos Kadis <avgoustinos.kadis@kdemail.net>
+     */
+    class KEduVocDocument : public QObject
+    {
+        Q_OBJECT
+        /// Read-only property of the active document
+        Q_PROPERTY(QObject * document READ getDocument)
+        /// Abreviation of document property (same as KEduVocDocument.document)
+        Q_PROPERTY(QObject * doc READ getDocument)
+        /// Currently active lesson
+        //Q_PROPERTY(QObject * activeLesson READ activeLesson)
+        //             Q_ENUMS ( Number Case Person Gender Definiteness )
+        Q_ENUMS(Flags)
+
+    public:
+
+        enum Flags {
+            // This is used for both empty flags and to denote no flags of the correct type were set.
+            NoInformation = 0x0,
+
+            // Gender
+            Masculine = 0x1,
+            Feminine = 0x2,
+            Neuter = 0x4,
+
+            // Plurality
+            Singular = 0x10,
+            Dual = 0x20,
+            Plural = 0x40,
+
+            // Part of Speech
+            Verb = 0x100,
+            Noun = 0x200,
+            Pronoun = 0x400,
+            Adjective = 0x800,
+            Adverb = 0x1000,
+            Article = 0x2000,
+            Conjunction = 0x4000,
+
+            // Person
+            First = 0x10000,
+            Second = 0x20000,
+            Third = 0x40000,
+
+            // Declension Case
+            Nominative = 0x80000,
+            Genitive = 0x100000,
+            Dative = 0x200000,
+            Accusative = 0x400000,
+            Ablative = 0x800000,
+            Locative = 0x1000000,
+            Vocative = 0x2000000,
+
+            // Other assorted flags
+            Definite = 0x4000000, // The article is definite
+            Indefinite = 0x8000000, // The article is indefinite
+            Regular = 0x10000000,
+            Irregular = 0x20000000
+        };
+
+        /**
+         * @param editor Represents a Editor::EditorWindow object
+         * @param doc Represents a KEduVococument object
+         *      For example, when using this function in Parley, pass the doc value like this:
+         *      m_editor->m_mainWindow->parleyDocument()->document()
+         *      where m_editor is a EditorWindow object
+         * @param vocabularyModel Represents the VocabularyModel object for the KEduVocDocument
+         *      For example, when using this function in Parley, pass the vocabularyModel value like this:
+         *      m_editor->m_vocabularyModel
+         *      where m_editor is a EditorWindow object
+         */
+        KEduVocDocument( ::KEduVocDocument * doc, KEduVocVocabularyModel * vocabularyModel );
+
+        ~KEduVocDocument();
+
+        /*
+         * Emits translateWord signal so the script function connected to it translate the @p word
+         * @param word Word to translate
+         * @param fromLanguage The language of @p word to translate from
+         * @param toLanguage The language you want to translate to
+         */
+        void callTranslateWord(const QString & word, const QString& fromLanguage, const QString& toLanguage);
+
+        //sets the translator object
+        //             void setTranslator ( Translator* translator ) { m_translator = translator; }
+        KEduVocTranslator * translator() {
+            return m_translator;
+        }
+
+        //Returns the active document
+        QObject* getDocument() {
+            return m_doc;
+        }
+
+        //Returns the active lesson
+        //KEduVocLesson* activeLesson();
+
+    public Q_SLOTS:
+
+        QStringList dataDirs() {
+            return QStandardPaths::standardLocations(QStandardPaths::DataLocation);
+        }
+
+        QStringList pluginDirs() {
+            QStringList basedirs(QStandardPaths::standardLocations(QStandardPaths::DataLocation));
+            QStringList ret;
+            foreach ( const QString dir , basedirs){
+                ret << (dir + "/plugins");
+            }
+
+            return ret;
+        }
+
+        /** close the document*/
+        void close();
+
+        /** Returns a list of Expression objects (the selected entries of the active lesson) 
+            Usage example:
+                While using in Parley, pass the 'indexes' value like this
+                indexes = m_editor->m_vocabularyView->getSelectedIndexes();
+                where m_editor is a EditorWindow object
+            TODO  Add a KEduVocVocabularyView object to this class (after is is moved to keduvoc from parley) and access getSelectedIndexes() from there
+         */
+        QVariantList selectedEntries( QModelIndexList indexes );
+
+        /** Returns a list of Translation objects (the selected translations of the active lesson) 
+            Usage example:
+                While using in Parley, pass the 'indexes' value like this
+                indexes = m_editor->m_vocabularyView->getSelectedIndexes();
+                where m_editor is a EditorWindow object
+            TODO  Add a KEduVocVocabularyView object to this class (after is is moved to keduvoc from parley) and access getSelectedIndexes() from there
+         */
+        QVariantList selectedTranslations( QModelIndexList indexes );
+
+        /**
+         * Adds the found @p translation of the @p word from language @p fromLanguage to language @p toLanguage to %KEduVocDocument translations to be used for translating lesson entries (or anything else). This function is ment to be used by scripts to add a translation of a word by parsing either online or offline dictionaries.
+         *
+         * @code
+         * #example usage of addTranslation function
+         * import KEduVocDocument
+         * #function called by KEduVocDocument whenever a translation of a word is needed
+         * def translateWord(word,fromLang,toLang):
+         *     <<look for the word translation>>
+         *     KEduVocDocument.addTranslation(word,fromLang,toLang,foundWord)
+         *     <<look for more translations>>
+         * @endcode
+         *
+         * @param word Translated word
+         * @param fromLanguage From language
+         * @param toLanguage To language
+         * @param translation %Translation of word
+         */
+        void addTranslation(QString word, QString fromLanguage, QString toLanguage, QString translation);
+        /**
+         * Returns a list of all available locales (to be used by the scripts)
+         * @return
+         */
+        QStringList locales();
+
+        /**
+         * Gives the language name of the given @p locale
+         * @param locale Language locale
+         * @return Language name
+         */
+        QString localeName(QString locale);
+
+        /// @todo Make this function working (not very important function)
+        /*
+         * Open the KEduVocDocument Document @p filename
+         * Usage:
+         * @code
+         * import KEduVocDocument
+         * KEduVocDocument.open("Vocab/MyVocab.kvtml")
+         * @endcode
+         * @param file KEduVocDocument Document file path (ex. /home/kde-user/MyVocab.kvtml)
+         */
+        bool open(QString filename);
+
+        /**
+         * Creates a new document and returns a reference to it
+         *
+         * @code
+         * #how to create a new document, add lessons, add entries and save it to a kvtml file
+         * import KEduVocDocument
+         *
+         * #create new document
+         * doc = KEduVocDocument.newDocument()
+         * doc.title = "New document"
+         *
+         * #set identifiers
+         * doc.appendNewIdentifier("English","en_US")
+         * doc.appendNewIdentifier("French","fr")
+         *
+         * #lessons
+         * l1 = doc.newLesson("Lesson1")
+         * doc.rootLesson.appendChildLesson(l1)
+         *
+         * #add a new entry (first way)
+         * e = l1.newEntry()
+         * e.setTranslation(0,"dog")
+         * e.setTranslation(1,"chien")
+         * l1.appendEntry(e)
+         *
+         * #add a new entry (second way)
+         * ee = l1.newEntry(["glass","verre"])
+         * l1.appendEntry(ee)
+         * #third way
+         * ee = l1.appendNewEntry(["book","livre"])
+         *
+         * #add a new lesson (fast way)
+         * l2 = doc.appendNewLesson("Lesson 2")
+         *
+         * #add a new child lesson under "Lesson 2"
+         * l3 = doc.appendNewLesson("Lesson 3",l2)
+         *
+         * #add a new entry (third way)
+         * l2.appendNewEntry(["I","je"]);
+         * l3.appendNewEntry(["good morning","bonjour"])
+         *
+         * #save document
+         * doc.saveAs("/home/kde-devel/test_new_document.kvtml")
+         * @endcode
+         *
+         * @return A Document object, the newly created lesson
+         */
+        QObject * newDocument() {
+            return new ::KEduVocDocument();
+        }
+
+
+    Q_SIGNALS:
+        /**
+         * Slots (script functions) connected to this signal are called when a translation of @p word is requested. Note that a script function with the same name as a signal will be automatically connected to that signal when the script is activated.
+         *
+         * @code
+         * #example usage of translateWord signal
+         * import KEduVocDocument
+         * #function called by KEduVocDocument whenever a translation of a word is needed
+         * def translateWord(word,fromLang,toLang):
+         *     <<look for the word translation>>
+         *     KEduVocDocument.addTranslation(word,fromLang,toLang,foundWord)
+         *     <<look for more translations>>
+         * @endcode
+         *
+         * @param word Word to translate
+         * @param fromLanguage The language of @p word to translate from
+         * @param toLanguage The language you want to translate to
+         */
+        void translateWord(const QString & word, const QString& fromLanguage, const QString& toLanguage);
+        /* Emits when the translation of a word is finished (from all the scripts) [not to be used by scripts] */
+        void translationFinished(const QString & word, const QString& fromLanguage, const QString& toLanguage);
+        /* Emits when the translation of a word starts [not to be used by scripts] */
+        void translationStarted(const QString & word, const QString& fromLanguage, const QString& toLanguage);
+        /** Emitted when the document pointer is changed. */
+        void documentChanged(::KEduVocDocument *newDocument);
+    private:
+        KEduVocTranslator* m_translator;
+        ::KEduVocDocument* m_doc;
+        KEduVocVocabularyModel * m_vocabularyModel;
+    };
+}
+
+Q_DECLARE_METATYPE( KEduVocTranslation* )
+
+#endif
